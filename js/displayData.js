d0a25fb086c113cfb252a03fb73ca07320a00ba6
JasperBleeker <jasper.bleeker@stud.fhgr.ch>
2024-12-13 14:06:33 +0100

    Frontend Style finished

--- js/displayData.js
+++ js/displayData.js
@@ -18,7 +18,7 @@ async function getData() {
 
 function displayData(data) {
     const container = document.getElementById('routes-container');
-    
+
     data.best_routes.forEach((route, routeIndex) => {
         const first = route[0];
         const last = route[route.length - 1];
@@ -28,8 +28,28 @@ function displayData(data) {
         routeContainer.classList.add('route');
 
         routeContainer.innerHTML = `
-            <h2>Route ${routeIndex + 1} ${first.type}</h2>
-            <p><strong>Von:</strong> ${first.from} <strong>(${first.departure})</strong> nach <strong>${last.to}</strong> <strong>(${last.arrival})</strong></p>
+        <div class="title">
+            <p>${route[0].from} → ${route[route.length - 1].to}<p>
+            <p>${route[0].type}</p>
+        </div>
+        <div class="route-info">
+            <p>Abfahrt:</p>
+            <div class="departure">
+                <p>${first.from}</p>
+                <p>Gleis: ${first.platform}</p>
+                <p>${first.departure}</p>
+            </div>
+            <svg width="308" height="2" viewBox="0 0 308 2" fill="none" xmlns="http://www.w3.org/2000/svg">
+<line x1="8.74228e-08" y1="1" x2="308" y2="1.00003" stroke="black" stroke-width="2"/>
+</svg>
+            <p>Ankunft:</p>
+            <div class="arrival">
+                <p>${last.to}</p>
+                <p>Gleis: ${last.platform}</p>
+                <p>${last.arrival}</p>
+            </div>
+        </div>
+        <button class="show-details">Reise Öffnen</button>
         `;
 
         container.appendChild(routeContainer);
